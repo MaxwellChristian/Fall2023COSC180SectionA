7ce14fa70988411fda4b8a08e177f387aee4c591
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-06 10:07:49 -0600

    Maxwell Christian

---
+++ src/CharacterDemo.java
@@ -0,0 +1,22 @@
+/*
+This program demonstrates the use of char data type
+ */
+public class CharacterDemo {
+
+    public static void main(String []args){
+
+        // declaration section
+        char ch;    // this is a variable of type char
+        // it can hold ONLY a SINGLE character
+
+        // input section
+        ch = 'M';
+
+        // processing section
+
+        // output section
+        System.out.println("The variable ch has: " + ch);
+
+    }
+
+}
