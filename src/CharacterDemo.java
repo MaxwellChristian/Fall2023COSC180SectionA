c99f75d63f68a65a729c1cf3b324633959ac15bd
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-06 10:17:04 -0600

    Maxwell Christian

--- src/CharacterDemo.java
+++ src/CharacterDemo.java
@@ -9,13 +9,38 @@ public class CharacterDemo {
         char ch;    // this is a variable of type char
         // it can hold ONLY a SINGLE character
 
+        int num;    // an int variable to store whole number
+
         // input section
-        ch = 'M';
+        ch = 'A';
 
         // processing section
+        num = ch;
 
         // output section
-        System.out.println("The variable ch has: " + ch);
+        System.out.println("The variable ch has  : " + ch);
+        System.out.println("The variable num has : " + num);
+
+        // ch = 'a';
+        // num = ch ;
+        num = ch = 'a';     // a multi-assignment statement
+        // similar to above-mentioned two individual assignment statements
+        System.out.println("The variable ch has  : " + ch);
+        System.out.println("The variable num has : " + num);
+
+        // ch = 'Z';
+        // num = ch ;
+        num = ch = 'Z';     // a multi-assignment statement
+        // similar to above-mentioned two individual assignment statements
+        System.out.println("The variable ch has  : " + ch);
+        System.out.println("The variable num has : " + num);
+
+        // ch = 'z';
+        // num = ch ;
+        num = ch = 'z';     // a multi-assignment statement
+        // similar to above-mentioned two individual assignment statements
+        System.out.println("The variable ch has  : " + ch);
+        System.out.println("The variable num has : " + num);
 
     }
 
