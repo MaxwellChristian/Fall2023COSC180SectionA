0d1b03cf49de5c8d192c2da627e07c9af661a3cc
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-14 15:00:30 -0600

    Maxwell Christian

--- src/lo_decision_statements/PrecedenceAndAssociativityDemo.java
+++ src/lo_decision_statements/PrecedenceAndAssociativityDemo.java
@@ -4,21 +4,7 @@ public class PrecedenceAndAssociativityDemo {
 
     public static void main(String []args){
 
-        if( 5 < 2 ) System.out.println("In true");
-        else System.out.println("In false");
 
-        if( 5 < 2 ) {System.out.println("In true");}
-        else {System.out.println("In false");}
-
-        if( 5 < 2 ) System.out.println("In true");
-        else {System.out.println("In false");}
-
-        if( 5 < 2 ) {System.out.println("In true");}
-        else System.out.println("In false");
-
-        if( 5 < 2 ) System.out.println("In true");
-        else
-            System.out.println("In false");
 
     }
 
