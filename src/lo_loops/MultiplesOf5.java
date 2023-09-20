d822053d1261c83563478d4d2d83bb4100d21d48
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-20 08:48:50 -0600

    Maxwell Christian

--- src/lo_loops/MultiplesOf5.java
+++ src/lo_loops/MultiplesOf5.java
@@ -1,5 +1,7 @@
 package lo_loops;
 
+import java.util.Scanner;
+
 public class MultiplesOf5 {
 
     public static void main(String []args) {
@@ -7,11 +9,24 @@ public class MultiplesOf5 {
         // a program to print multiples of 5 between
         // the range specified by user
 
+        // input line
+        Scanner inputLine = new Scanner(System.in);
+
+        // prompt the user for starting point
+        System.out.print("Enter the start number: ");
+        int startNumber = inputLine.nextInt();
+
+        // prompt the user for ending point
+        System.out.print("Enter the end number: ");
+        int endNumber = inputLine.nextInt();
+
         // start point
-        int counter = 1;
+        //int counter = 1;
+        int counter = startNumber;
 
         // check point
-        while ( counter <= 250 ) {
+//        while ( counter <= 250 ) {
+        while ( counter <= endNumber ) {
 
             // check if the current counter is a multiple of 5
             if( counter%5 == 0 ){
