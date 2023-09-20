c6992ad2ee8c89b9056b3a1441bf953ca7ffb4de
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-20 09:39:23 -0600

    Maxwell Christian

--- src/lo_loops/KgToPounds.java
+++ src/lo_loops/KgToPounds.java
@@ -4,18 +4,18 @@ public class KgToPounds {
 
     public static void main(String []args) {
 
-        int counter ;
+        int kg ;
 
         // start value
-        counter = 1;
+        kg = 1;
 
         // check condition
-        while ( counter <= 199 ) {
+        while ( kg <= 199 ) {
 
-            System.out.printf("%3d : %5.1f\n", counter, counter*2.2);
+            System.out.printf("%3d : %5.1f\n", kg, kg*2.2);
 
             // change value
-            counter += 2;
+            kg += 2;
         }
 
     }
