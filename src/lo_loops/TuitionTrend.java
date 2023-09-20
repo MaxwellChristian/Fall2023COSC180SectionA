21d271aaaffe19ad2d5cb9850fe0edecd92b0a1a
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-20 09:43:00 -0600

    Maxwell Christian

---
+++ src/lo_loops/TuitionTrend.java
@@ -0,0 +1,21 @@
+package lo_loops;
+
+
+/*
+CST Tuition is allowed to increase at annual rate of 3.5%.
+Given that tuition is 9610 – print out the tuition costs for the next 10 years.
+ */
+public class TuitionTrend {
+
+    public static void main(String []args) {
+
+        int year;
+        double tution = 9610;
+
+        for( year = 2023 ; year <= (year+10) ; year+=1 ){
+            System.out.printf("%4d : %d\n", year, 9610 + () );
+        }
+
+    }
+
+}
