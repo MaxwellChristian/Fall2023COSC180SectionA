255bf01a8f2a3c21a5e2ffc5c9dcf6efbdbfb839
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-09-28 09:48:32 -0600

    Maxwell Christian

---
+++ src/lo_methods/PrimeNumbers.java
@@ -0,0 +1,58 @@
+package lo_methods;
+
+/*
+* Write a program which takes input of two numbers and displays
+* all the numbers that are prime numbers
+*
+* Prime numbers are numbers which are divisible ONLY by 1 and itself
+* i.e. does not have any factors
+*
+* perform the check for prime using a method
+* */
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static void main(String []args){
+
+        int startNumber;
+        int endNumber;
+
+        Scanner inputLine = new Scanner(System.in);
+
+        System.out.print("Enter start number: ");
+        startNumber = inputLine.nextInt();
+
+        System.out.print("Enter end number: ");
+        endNumber = inputLine.nextInt();
+
+        for( int counter = startNumber ; counter <= endNumber ; counter++ ){
+
+            // check if the counter is prime
+            // boolean result = isPrime(counter);
+
+            // if(result){
+                // System.out.println(counter + " is prime");
+            // }
+
+            if(isPrime(counter)){
+                System.out.println(counter + " is prime");
+            }
+
+        }
+
+    }
+
+    private static boolean isPrime(int value) {
+
+        for(int divisor = 2 ; divisor < value ; divisor++){
+            if( value%divisor == 0 ){
+                return  false;
+            }
+        }
+
+        return true;
+    }
+
+}
