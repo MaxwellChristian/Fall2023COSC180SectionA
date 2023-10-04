88ab289158c8c79a59f8bf878a49a2971a1e17c5
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-10-04 09:49:43 -0600

    Maxwell Christian

--- src/lo_methods/DigitMethods.java
+++ src/lo_methods/DigitMethods.java
@@ -20,6 +20,44 @@ public class DigitMethods {
         int count = countOfDigits(number);
         System.out.println("Count of digits in " + number + " : " + count );
 
+        count = (Math.abs(number) + "").length();
+        System.out.println("Count of digits in " + number + " : " + count );
+
+        int sum = sumOfDigits(number);
+        System.out.println("Sum of digits in " + number + " : " + sum );
+
+        int reversed = reverseDigits(number);
+        System.out.println("Sum of digits in " + number + " : " + reversed );
+
+        String reversedStr = new StringBuilder(""+number).reverse().toString();
+        System.out.println("Sum of digits in " + number + " : " + reversedStr );
+
+
+
+    }
+
+    private static int reverseDigits(int number) {
+        int reversed = 0;
+
+        for( reversed = 0; number != 0 ; number/=10 ){
+            int dig = number % 10 ;
+
+            reversed *= 10;
+            reversed += dig;
+        }
+
+        return reversed;
+    }
+
+    private static int sumOfDigits(int number) {
+
+        int sum;
+        for( sum = 0; number != 0 ; number/=10 ){
+            int dig = number % 10 ;
+            sum += dig;
+        }
+
+        return sum;
     }
 
     private static int countOfDigits(int number) {
@@ -35,8 +73,16 @@ public class DigitMethods {
 //
 //        }
 
-        int counter;
-        for( counter = 0; number > 0 ; number/=10, counter++ );
+        int counter = 0;
+
+        if( number == 0 ){
+            return 1;
+        }
+        else {
+            for( counter = 0; number != 0 ; number/=10, counter++ );
+        }
+
+
 
 
         // {
