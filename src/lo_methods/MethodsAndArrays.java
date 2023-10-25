6fb52831bfd78c0e7f2118132110da7ecf68c8f1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-10-25 09:33:36 -0600

    Maxwell Christian

--- src/lo_methods/MethodsAndArrays.java
+++ src/lo_methods/MethodsAndArrays.java
@@ -7,25 +7,29 @@ public class MethodsAndArrays {
     public static void main(String []args) {
 
         // call/invoke a method which returns an array filled with random numbers
-        int []values = generateRandomArray(20, 100);
+        int []values = generateRandomArray(20, 10);
         int []binaryValues = generateRandomArray(20);
 
         System.out.println(Arrays.toString(values));
         System.out.println(Arrays.toString(binaryValues));
 
-        boolean has55 = arrContains(values, 55);
-        System.out.println("55 exists in array : " + has55 );
+//        boolean has3 = arrContains(values, 3) > 0;
+        System.out.println("3 exists in array : " + (arrContains(values, 3) > 0) );
 
+        // modify/add the function to count the total times 55 occurs in random generated array
+        int count3 = arrContains(values, 3);
+        System.out.println("3 occurs in array " + count3 + " times " );
     }
 
-    private static boolean arrContains(int[] values, int valueToCheck) {
+    private static int arrContains(int[] values, int valueToCheck) {
 
+        int count = 0;
         for (int currentValue: values){
             if( currentValue == valueToCheck ){
-                return true;
+                count++;
             }
         }
-        return false;
+        return count;
     }
 
     private static int[] generateRandomArray(int totalValues, int maxValuePermitted) {
