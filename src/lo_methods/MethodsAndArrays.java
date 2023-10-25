152b059481c80f8007bea2da824f9060b956cdce
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-10-25 09:57:42 -0600

    Maxwell Christian

--- src/lo_methods/MethodsAndArrays.java
+++ src/lo_methods/MethodsAndArrays.java
@@ -4,29 +4,35 @@ import java.util.Arrays;
 
 public class MethodsAndArrays {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
         // call/invoke a method which returns an array filled with random numbers
-        int []values = generateRandomArray(20, 10);
-        int []binaryValues = generateRandomArray(20);
+        int[] values = generateRandomArray(20, 10);
+        int[] binaryValues = generateRandomArray(20);
 
         System.out.println(Arrays.toString(values));
         System.out.println(Arrays.toString(binaryValues));
 
 //        boolean has3 = arrContains(values, 3) > 0;
-        System.out.println("3 exists in array : " + (indexInArray(values, 3) != -1) );
+        System.out.println("3 exists in array : " + (indexInArray(values, 3) != -1));
 
         // modify/add the function to count the total times 55 occurs in random generated array
         int count3 = arrContains(values, 3);
-        System.out.println("3 occurs in array " + count3 + " times " );
+        System.out.println("3 occurs in array " + count3 + " times ");
 
         int positionFound = indexInArray(values, 3);
+        if (positionFound != -1) {
+            System.out.println("3 found at : " + positionFound);
+        } else {
+            System.out.println("3 does not exist in the array");
+        }
+
     }
 
     private static int indexInArray(int[] values, int valueToCheck) {
         int position = 0;
-        for (int currentValue: values){
-            if( currentValue == valueToCheck ){
+        for (int currentValue : values) {
+            if (currentValue == valueToCheck) {
                 return position;
             }
             position++;
@@ -37,8 +43,8 @@ public class MethodsAndArrays {
     private static int arrContains(int[] values, int valueToCheck) {
 
         int count = 0;
-        for (int currentValue: values){
-            if( currentValue == valueToCheck ){
+        for (int currentValue : values) {
+            if (currentValue == valueToCheck) {
                 count++;
             }
         }
@@ -46,9 +52,9 @@ public class MethodsAndArrays {
     }
 
     private static int[] generateRandomArray(int totalValues, int maxValuePermitted) {
-        int []arrToReturn = new int[totalValues];
-        for( int index = 0 ; index < arrToReturn.length ; index++ ){
-            arrToReturn[index] = (int) (Math.random()*maxValuePermitted);
+        int[] arrToReturn = new int[totalValues];
+        for (int index = 0; index < arrToReturn.length; index++) {
+            arrToReturn[index] = (int) (Math.random() * maxValuePermitted);
         }
 
         return arrToReturn;
