2740666244907dc56762651b9d1f223eb7357585
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-11-06 09:34:41 -0600

    Maxwell Christian

--- src/lo_oop/Fan.java
+++ src/lo_oop/Fan.java
@@ -23,9 +23,84 @@ along with the string “fan is off” in one combined string.
 Write a test program
 that creates two Fan objects. Assign maximum speed, radius 10, color yellow,
 and turn it on to the first object. Assign medium speed, radius 5, color blue, and
-turn it off to the second object. Display the objects by invoking their toString
-method.
+turn it off to the second object.
+
+Display the objects by invoking their toString method.
 
  */
 public class Fan {
+
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        speed = SLOW;
+        on = false;
+        radius = 5;
+        color = "blue";
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void turnOn() {
+        this.on = true;
+    }
+
+    public void turnOff() {
+        this.on = false;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+
+        if( this.isOn() ){
+            return "Fan{" +
+                    "speed=" + speed +
+                    ", on=" + on +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+        else {
+            return "Fan{" +
+                    "Fan is OFF" +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+
+
+    }
 }
