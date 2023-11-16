603f49394122fdcc5c7bfa23b4cb53876358f2fe
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-11-15 18:27:17 -0600

    Maxwell Christian

--- src/doubt_solving_session_1/Demo_Arrays_Methods_3.java
+++ src/doubt_solving_session_1/Demo_Arrays_Methods_3.java
@@ -21,5 +21,31 @@ public class Demo_Arrays_Methods_3 {
 
         int []allPrimes = MyDemoMethods.filterAllPrimes( arrValues );
         System.out.println("Filtered prime values: " + Arrays.toString(allPrimes));
+
+        int [][]values2D = new int[][]{
+                {11, 12, 13,14},
+                {15, 17, 18}
+        };
+
+        int totalValues = countValues(values2D);
+        System.out.println("Total elements in 2D: " + totalValues);
+
+    }
+
+    private static int countValues(int[][] values2D) {
+
+        int total = 0;
+        for( int r = 0 ; r < values2D.length ; r++ ){
+            for( int c = 0 ; c < values2D[r].length ; c++ ){
+
+//                if( MyDemoMethods.checkForPrime( values2D[r][c] ) ){
+//
+//                }
+
+                total++;
+            }
+        }
+
+        return total;
     }
 }
