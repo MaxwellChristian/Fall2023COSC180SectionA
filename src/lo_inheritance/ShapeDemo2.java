16414c21d9646d4b0c85ab3780913548e63359fd
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-11-29 09:55:20 -0600

    Maxwell Christian

---
+++ src/lo_inheritance/ShapeDemo2.java
@@ -0,0 +1,72 @@
+package lo_inheritance;
+
+import lo_inheritance.multi_level.C;
+import org.w3c.dom.css.Rect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ShapeDemo2 {
+
+    public static void main(String []args){
+
+        // create multiple shapes
+        // 30 circles
+        // 20 rectangles
+
+        ArrayList<Shape> shapes = new ArrayList<>();
+        // there is no size restriction on array list
+        // it expands as and when you add objects to it
+
+        shapes.add(new Circle(5));
+        shapes.add(new Circle(1));
+        shapes.add(new Circle(1));
+
+        shapes.add(new Rectangle(11, 15));
+        shapes.add(new Rectangle(12, 17));
+
+        // display the first object from the array list
+        System.out.println(shapes.get(0));
+
+        // display the last object from the array list
+        System.out.println(shapes.get( shapes.size() - 1 ));
+
+        // display total shapes
+        System.out.println("Total shapes: " + shapes.size());
+
+        // draw each shape
+        for(Shape s: shapes){
+            s.draw();   // the polymorphic method draw
+        }
+
+        shapes.add(new Circle(7));
+        shapes.add(new Rectangle(2, 1));
+
+        // display total shapes
+        System.out.println("Total shapes: " + shapes.size());
+
+        // remove the shape at index 2
+        shapes.remove(2);
+
+        // display total shapes
+        System.out.println("After removing: Total shapes: " + shapes.size());
+
+        // show the shape at index 2;
+        System.out.print("Shape at index 2: " + shapes.get(2));
+
+        shapes.set(2, new Circle(18));
+        // show the shape at index 2;
+        System.out.print("Shape at index 2: " + shapes.get(2));
+        // display total shapes
+        System.out.println("Total shapes: " + shapes.size());
+
+        // show all shapes
+        for(Shape s: shapes){
+            System.out.println(s);
+        }
+
+        // sort all the shapes
+
+    }
+
+}
