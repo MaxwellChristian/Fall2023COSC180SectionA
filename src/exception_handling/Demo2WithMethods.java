e219b6eba57fd80681c825f27ed5926306ce648a
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-04 10:54:14 -0600

    Maxwell Christian

--- src/exception_handling/Demo2WithMethods.java
+++ src/exception_handling/Demo2WithMethods.java
@@ -11,6 +11,7 @@ package exception_handling;
  *   All the steps 1, 2 and 3 should be done using methods
  * */
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Demo2WithMethods {
@@ -19,33 +20,48 @@ public class Demo2WithMethods {
 
         Scanner inputLine = new Scanner(System.in);
 
-        // input the array size
-        System.out.print("Enter array size: ");
-        int arrSize = inputLine.nextInt();
+        try {
+            // input the array size
+            System.out.print("Enter array size: ");
+            int arrSize = inputLine.nextInt();
 
-        // create the array as per size
-        int[] arr = createArray(arrSize);
+            // create the array as per size
+            int[] arr = createArray(arrSize);
+            System.out.println("Array creation completed");
 
-        // initialize the array elements to random numbers upto 5
-        initializeArray(arr, 5);
+            // initialize the array elements to random numbers upto 5
+            initializeArray(arr, 5);
+            System.out.println("Array initialization completed");
 
-        // divide the value at an index by the value at next index
-        showDivisionResult(arr);
+            // divide the value at an index by the value at next index
+            showDivisionResult(arr);
+            System.out.println("Arithmetic operations completed");
+
+            System.out.println("Bye");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
 
     }
 
     private static void showDivisionResult(int []arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int result = arr[i] / arr[i + 1];
 
-            // display the result
-            System.out.printf("%d / %d : %d\n", arr[i], arr[i + 1], result);
+            try {
+                int result = arr[i] / arr[i + 1];
+
+                // display the result
+                System.out.printf("%d / %d : %d\n", arr[i], arr[i + 1], result);
+            } catch ( ArithmeticException ex ) {
+                System.out.println("Trying to divide by zero: Skipped");
+            }
+
         }
     }
 
     private static void initializeArray(int[] arr, int maxValue) {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() % maxValue);
+            arr[i] = new Random().nextInt(maxValue);
         }
     }
 
@@ -54,9 +70,9 @@ public class Demo2WithMethods {
             return new int[arrSize];
         } catch (NegativeArraySizeException ex) {
             System.out.println("Array size cannot be negative");
+            throw ex;
         }
 
-        return new int[0];
     }
 
 }
