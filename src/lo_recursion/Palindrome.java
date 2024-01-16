903ba59a51a5365f38f72fb56007862d12c2cfc6
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-16 11:37:38 -0600

    Maxwell Christian

--- src/lo_recursion/Palindrome.java
+++ src/lo_recursion/Palindrome.java
@@ -5,4 +5,27 @@ package lo_recursion;
 * */
 
 public class Palindrome {
+
+    public static void main(String []args) {
+
+        for (String word : args) {
+            System.out.println(word + " : Palindrome : " + isPalindrome(word)  );
+        }
+
+    }
+
+    private static boolean isPalindrome(String word) {
+
+        if( word.length() <= 1 ){
+            return true;
+        } else {
+            if( word.charAt(0) != word.charAt(word.length()-1) ){
+                return false;
+            } else {
+                return isPalindrome(word.substring(1, word.length()-1));
+            }
+        }
+
+    }
+
 }
