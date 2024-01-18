3cee50571811c76092268cd07b901259c0b9dba1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-18 10:49:10 -0600

    Maxwell Christian

--- src/lo_generics/GenericsDemo.java
+++ src/lo_generics/GenericsDemo.java
@@ -1,5 +1,8 @@
 package lo_generics;
 
+import lo_inheritance.Circle;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GenericsDemo {
@@ -9,14 +12,38 @@ public class GenericsDemo {
         Integer []iValues = {11, 22, 33, 44, 55};
         String []sValues = {"AA", "BB", "CC" };
 
-        showIntValues(iValues);
-        showTextValues(sValues);
+        Circle []c = new Circle[3];
+        c[0] = new Circle(11);
+        c[1] = new Circle(22);
+        c[2] = new Circle(33);
 
+//        showIntValues(iValues);
+//        showTextValues(sValues);
+
+        showValues(iValues);
+        showValues(sValues);
+
+        showValues(c);
 
     }
 
+    private static <E> void showValues(E []values) {
+        for (E value: values) {
+            System.out.println(value);
+        }
+    }
 
+    private static void showTextValues(String[] sValues) {
+        for (String value: sValues) {
+            System.out.println(value);
+        }
+    }
 
+    private static void showIntValues(Integer[] iValues) {
+        for (Integer value: iValues) {
+            System.out.println(value);
+        }
+    }
 
 
 }
