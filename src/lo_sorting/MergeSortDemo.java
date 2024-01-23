028a170821a066421dec138b8eac371d32804d19
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-23 11:48:04 -0600

    Maxwell Christian

--- src/lo_sorting/MergeSortDemo.java
+++ src/lo_sorting/MergeSortDemo.java
@@ -16,4 +16,37 @@ public class MergeSortDemo {
 
     }
 
+    private static void mergeSort(int[] values) {
+
+        // base case
+        if (values.length > 1) {
+
+            // merge sort the left half of the list
+            int[] leftList = new int[values.length / 2];
+            mergeSort(leftList);
+
+            // merge sort the second half of the list
+            int[] rightList = new int[values.length - values.length / 2];
+            mergeSort(rightList);
+
+            // merge first and second halves into a single list
+            merge(leftList, rightList, values);
+        }
+
+    }
+
+    private static void merge(int[] leftList, int[] rightList, int[] finalList) {
+
+
+        // try to merge each value from both lists
+            // compare the values and decide which value to copy to final list
+
+
+        // copy the remaining values from left list
+
+
+        // copy the remaining values from the right list
+
+    }
+
 }
