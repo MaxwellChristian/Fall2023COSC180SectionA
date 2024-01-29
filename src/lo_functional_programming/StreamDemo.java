763b451b7da82159e110e8d23c6e98b9e5b795e5
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-29 12:11:01 -0600

    Maxwell Christian

--- src/lo_functional_programming/StreamDemo.java
+++ src/lo_functional_programming/StreamDemo.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 
 public class StreamDemo {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
-        String []words = { "Hello", "World", "How", "are", "You", "How", "are", "you", "all", "today" } ;
+        String[] words = {"Hello", "World", "How", "are", "You", "How", "are", "you", "all", "today"};
 
         // display first 4 words
         System.out.println("First 4 words: " + Arrays.stream(words).limit(4).toList());
@@ -28,14 +28,51 @@ public class StreamDemo {
         System.out.println("Words [length>=4]: " + Arrays.stream(words).filter(s -> s.length() >= 4).toList());
 
         // get the word with largest length from all words with length >= 4
+        System.out.println("Largest word with length >= 4: " + Arrays.stream(words).filter(s -> s.length() >= 4).max(String::compareTo).get());
 
         // get the smallest word alphabetically
+        System.out.println("Smallest word [alphabetically]: " + Arrays.stream(words).min(String::compareTo).get());
 
         // find if the word 'today' exists
+        // System.out.println("Exists 'today': " + Arrays.stream(words).anyMatch(s -> s.equals("today")));
+        // System.out.println("Exists 'today': " + Arrays.asList(words).contains("today"));
 
-        // display all names starting with a capital character
+        System.out.println("Exists 'today': " + Arrays.stream(words).anyMatch(s -> s.equalsIgnoreCase("today")));
+
+        // display all words starting with a capital character
+        System.out.println("All words starting with capital character: " + Arrays.stream(words).filter(s -> Character.isUpperCase(s.charAt(0))).toList());
+
+        // check if all words start with a capital character
+        System.out.println("All words starting with capital character: " + Arrays.stream(words).allMatch(s -> Character.isUpperCase(s.charAt(0))));
 
         // display words which does not start with 'ho'
+        System.out.println("Words not starting with 'ho': " + Arrays.stream(words).filter(s -> !s.toLowerCase().startsWith("ho")).toList());
+        System.out.println("Words not starting with 'ho': " + Arrays.stream(words).noneMatch(s -> s.startsWith("ho")));
+
+        // display all words starting with a capital character [sorted alphabetically]
+        // display the word with its length
+        System.out.println("All words starting with capital character: ");
+        Arrays.stream(words)
+                .filter(s -> Character.isUpperCase(s.charAt(0)))
+                .sorted()
+                .distinct()
+                .forEach(s -> System.out.printf("%s(%d)\n", s, s.length()));
+
+        // display the first word [in lower-case]
+        System.out.println("First word [in lower case]: " + Arrays
+                .stream(words)
+                .map(String::toLowerCase)
+                .findFirst()
+                .get()
+        );
+
+        // display the first lower-case word
+        System.out.println("First lower case word: " + Arrays
+                .stream(words)
+                .filter(s -> s.equals(s.toLowerCase()))
+                .findFirst()
+                .get()
+        );
     }
 
 }
