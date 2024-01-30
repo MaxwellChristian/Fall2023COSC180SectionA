b3698dd308f03d0ad04f9e47f93a6d997b0324a1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-30 11:48:02 -0600

    Maxwell Christian

---
+++ src/lo_functional_programming/StreamDemo4.java
@@ -0,0 +1,19 @@
+package lo_functional_programming;
+
+public class StreamDemo4 {
+
+    public static void main(String []args) {
+
+        String[] names = {"John", "Peter", "Susan", "Kim", "Jen",
+                "George", "Alan", "Stacy", "Michelle", "john"};
+
+        // count total characters across all names
+
+        // display all names in upper case
+        // should not have repeated names
+
+        // return all values as an array list
+
+    }
+
+}
