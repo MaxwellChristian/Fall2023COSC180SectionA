08e7db617845590614f69f29a85b91e176676673
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-01-30 11:07:53 -0600

    Maxwell Christian

---
+++ src/lo_functional_programming/StreamDemo3.java
@@ -0,0 +1,21 @@
+package lo_functional_programming;
+
+import java.util.Arrays;
+
+public class StreamDemo3 {
+
+    public static void main(String []args) {
+
+        int[] values = {3, 4, 1, 5, 20, 1, 3, 3, 4, 6};
+
+        System.out.println("Values: " + Arrays.toString(values));
+
+        // adding each value i.e. ans = v1 + v2 ... + vN
+
+        // multiplying each value i.e. ans = v1 * v2 ... * vN
+
+        // display as CSV
+
+    }
+
+}
