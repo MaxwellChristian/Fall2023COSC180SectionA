dd015233c63cf54b2541db69f51696dbf78f9c30
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-02-05 11:33:23 -0600

    Maxwell Christian

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        // task 1 : print 100000 'A' chars
+        // task 2 : print 100000 'B' chars
+        // task 3 : print 100000 int values
+
+        // display the time consumed in total for all tasks
+    }
+
+}
