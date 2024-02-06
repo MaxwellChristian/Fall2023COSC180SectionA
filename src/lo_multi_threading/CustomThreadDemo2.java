3686db4173fa7d5399f5c927c6c2e022f7ac08f8
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-02-06 10:56:14 -0600

    Maxwell Christian

--- src/lo_multi_threading/CustomThreadDemo2.java
+++ src/lo_multi_threading/CustomThreadDemo2.java
@@ -4,9 +4,9 @@ public class CustomThreadDemo2 {
 
     public static void main(String ... args) {
 
-        CustomThread2 cThread1 = new CustomThread2("C1", 100);
-        CustomThread2 cThread2 = new CustomThread2("C2", 100);
-        CustomThread2 cThread3 = new CustomThread2("C3", 100);
+        CustomThread2 cThread1 = new CustomThread2("C1", 100, 0);
+        CustomThread2 cThread2 = new CustomThread2("C2", 100, 0);
+        CustomThread2 cThread3 = new CustomThread2("C3", 100, 0);
 
         cThread1.start();
         cThread2.start();
