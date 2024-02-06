7fd3bbac0a88bdc32dbbd8529ef89b655760b1cf
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-02-06 11:13:35 -0600

    Maxwell Christian

--- src/lo_multi_threading/ThreadPoolDemo.java
+++ src/lo_multi_threading/ThreadPoolDemo.java
@@ -7,7 +7,8 @@ public class ThreadPoolDemo {
 
     public static void main(String []args) {
 
-        // a pool of threads [with maximum three threads]
+        // a pool of threads
+        // [with maximum specified threads to perform all required tasks]
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         // provide the runnable task to the executor
@@ -15,8 +16,16 @@ public class ThreadPoolDemo {
         executorService.execute(new CustomThread("Th2"));
         executorService.execute(new CustomThread("Th3"));
 
+        executorService.execute(new CustomThread("Th4"));
+        executorService.execute(new CustomThread("Th5"));
+
         // close/shutdown the executor service
         executorService.shutdown();
+
+        // wait till all threads are completed
+        while (!executorService.isTerminated());
+
+        System.out.println("Main: Completed");
 
     }
 
