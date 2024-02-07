fd7d2817d5d9cb9013d79981eee87e1124b0643f
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-02-07 10:18:48 -0600

    Maxwell Christian

---
+++ src/lo_multi_threading/consumer_producer/ConsumerProducerUsingBlockingQueue.java
@@ -0,0 +1,38 @@
+package lo_multi_threading.consumer_producer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConsumerProducerUsingBlockingQueue {
+
+    private static ArrayBlockingQueue<Integer> buffer = new ArrayBlockingQueue<>(2);
+
+    public static void main(String ... args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.execute(new ProducerTask());
+        executorService.execute(new ConsumerTask());
+
+        executorService.shutdown();
+    }
+
+    private static class ProducerTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                int count = 1;
+                while (true) {
+                    System.out.println("Produced: " + count);
+                    buffer.put(count++);
+                    Thread.sleep((long) (Math.random() * 2000));
+                }
+            } catch (InterruptedException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+}
