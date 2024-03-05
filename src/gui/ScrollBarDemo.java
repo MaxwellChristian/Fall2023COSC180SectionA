a008c5f0583d1f1cdf326126c1b0dad93cb643e1
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-03-05 12:30:15 -0600

    Maxwell Christian

--- src/gui/ScrollBarDemo.java
+++ src/gui/ScrollBarDemo.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ScrollBarDemo extends Application {
@@ -17,15 +18,32 @@ public class ScrollBarDemo extends Application {
         ScrollBar sbHorizontal = new ScrollBar();
         sbHorizontal.setOrientation(Orientation.HORIZONTAL);
         sbHorizontal.setMin(20);
-        sbHorizontal.setMax(100);
+        sbHorizontal.setMax(200);
         sbHorizontal.setUnitIncrement(10);
         sbHorizontal.setBlockIncrement(30);
 
-        sbHorizontal.valueProperty().addListener(observable -> System.out.println(sbHorizontal.getValue()));
+        sbHorizontal.valueProperty().addListener(observable -> {
+            System.out.println(sbHorizontal.getValue());
 
+            if (label.getTranslateX() + label.getWidth() + sbHorizontal.getValue() < primaryStage.getWidth()) {
+                label.setTranslateX(sbHorizontal.getValue());
+            }
+
+
+        });
 
         ScrollBar sbVertical = new ScrollBar();
         sbVertical.setOrientation(Orientation.VERTICAL);
+        sbVertical.setMin(label.getFont().getSize());
+        sbVertical.setMax(80);
+        sbVertical.setUnitIncrement(5);
+        sbVertical.setBlockIncrement(10);
+
+        // sbVertical.setVisible(false);
+
+        sbVertical.valueProperty().addListener(observable -> {
+            label.setFont(Font.font(sbVertical.getValue()));
+        });
 
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(label);
