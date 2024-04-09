28266ed14f6ab67b57673244d62136c2f66a8158
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2024-04-09 11:10:46 -0600

    Maxwell Christian

---
+++ src/main/java/lo_networking/tic_tac_toe/TicTacToeConstants.java
@@ -0,0 +1,13 @@
+package lo_networking.tic_tac_toe;
+
+public interface TicTacToeConstants {
+    int PLAYER1 = 1;
+    int PLAYER2 = 2;
+
+    int PLAYER1_WON = 1;
+    int PLAYER2_WON = 2;
+
+    int DRAW = 3;
+
+    int CONTINUE = 4;
+}
